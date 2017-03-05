005d07023481305a6a44cce667f98c6b0136b409
John-Mainsys <john@faversham-mainsys>
2017-03-05 09:47:45 +0000

    removed PATCH/POST and defaults so I can get the schema to load on AWS gateway

--- src/ruby/meetings/app/controllers/api/v1/people_controller.rb
+++ src/ruby/meetings/app/controllers/api/v1/people_controller.rb
@@ -24,12 +24,12 @@ class Api::V1::PeopleController < JSONAPI::ResourceController
           end
         end
       end
-      response :default do
-        key :description, 'unexpected error'
-        schema do
-          key :'$ref', :ErrorModel
-        end
-      end
+      # response :default do
+      #   key :description, 'unexpected error'
+      #   schema do
+      #     key :'$ref', :ErrorModel
+      #   end
+      # end
     end
   end
 
@@ -50,101 +50,101 @@ class Api::V1::PeopleController < JSONAPI::ResourceController
           end
         end
       end
-      response :default do
-        key :description, 'unexpected error'
-        schema do
-          key :'$ref', :ErrorModel
-        end
-      end
+      # response :default do
+      #   key :description, 'unexpected error'
+      #   schema do
+      #     key :'$ref', :ErrorModel
+      #   end
+      # end
     end
   end
 
-  swagger_path '/people' do
-    operation :post do
-      key :description, 'Create a new person object'
-      key :tags, [
-        'person'
-      ]
+  # swagger_path '/people' do
+  #   operation :post do
+  #     key :description, 'Create a new person object'
+  #     key :tags, [
+  #       'person'
+  #     ]
 
-      parameter do
-        key :name, :name
-        key :in, :body
-        key :description, 'Name of the person to add'
-        key :required, true
-        key :type, :string
-      end
-      parameter do
-        key :name, :wikipedia_entry
-        key :in, :body
-        key :description, 'The Wikipedia entry of the person to add'
-        key :required, false
-        key :type, :string
-      end
-      response 200 do
-        key :description, 'person response'
-        schema do
-          property :data do
-            key :'$ref', :Person
-          end
-        end
-      end
-      response :default do
-        key :description, 'unexpected error'
-        schema do
-          key :'$ref', :ErrorModel
-        end
-      end
-    end
-  end
+  #     parameter do
+  #       key :name, :name
+  #       key :in, :body
+  #       key :description, 'Name of the person to add'
+  #       key :required, true
+  #       key :type, :string
+  #     end
+  #     parameter do
+  #       key :name, :wikipedia_entry
+  #       key :in, :body
+  #       key :description, 'The Wikipedia entry of the person to add'
+  #       key :required, false
+  #       key :type, :string
+  #     end
+  #     response 200 do
+  #       key :description, 'person response'
+  #       schema do
+  #         property :data do
+  #           key :'$ref', :Person
+  #         end
+  #       end
+  #     end
+  #     # response :default do
+  #     #   key :description, 'unexpected error'
+  #     #   schema do
+  #     #     key :'$ref', :ErrorModel
+  #     #   end
+  #     # end
+  #   end
+  # end
 
-  swagger_path '/people' do
-    operation :patch do
-      key :description, 'Update a person object'
-      key :tags, [
-        'person'
-      ]
-      parameter do
-        key :name, :id
-        key :in, :path
-        key :description, 'ID of person to update'
-        key :required, true
-        key :type, :integer
-        key :format, :int64
-      end
-      parameter do
-        key :name, :id
-        key :in, :body
-        key :description, 'The id of the person to update'
-        key :required, true
-        key :type, :string
-      end
-      parameter do
-        key :name, :name
-        key :in, :body
-        key :description, 'name of the person to update'
-        key :required, false
-        key :type, :object
-      end
-      parameter do
-        key :name, :wikipedia_entry
-        key :in, :body
-        key :description, 'The Wikipedia entry of the person to update'
-        key :required, false
-        key :type, :string
-      end
-      response 200 do
-        key :description, 'person response'
-        schema do
-          key :'$ref', :Person
-        end
-      end
-      response :default do
-        key :description, 'unexpected error'
-        schema do
-          key :'$ref', :ErrorModel
-        end
-      end
-    end
-  end
+  # swagger_path '/people' do
+  #   operation :patch do
+  #     key :description, 'Update a person object'
+  #     key :tags, [
+  #       'person'
+  #     ]
+  #     parameter do
+  #       key :name, :id
+  #       key :in, :path
+  #       key :description, 'ID of person to update'
+  #       key :required, true
+  #       key :type, :integer
+  #       key :format, :int64
+  #     end
+  #     parameter do
+  #       key :name, :id
+  #       key :in, :body
+  #       key :description, 'The id of the person to update'
+  #       key :required, true
+  #       key :type, :string
+  #     end
+  #     parameter do
+  #       key :name, :name
+  #       key :in, :body
+  #       key :description, 'name of the person to update'
+  #       key :required, false
+  #       key :type, :object
+  #     end
+  #     parameter do
+  #       key :name, :wikipedia_entry
+  #       key :in, :body
+  #       key :description, 'The Wikipedia entry of the person to update'
+  #       key :required, false
+  #       key :type, :string
+  #     end
+  #     response 200 do
+  #       key :description, 'person response'
+  #       schema do
+  #         key :'$ref', :Person
+  #       end
+  #     end
+  #     response :default do
+  #       key :description, 'unexpected error'
+  #       schema do
+  #         key :'$ref', :ErrorModel
+  #       end
+  #     end
+  #   end
+  # end
 
 end
