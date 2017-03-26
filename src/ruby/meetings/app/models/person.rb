320f492a7eb4738dfd6d4200a6489c33dd132daf
John Small <john.small@bbc.com>
2017-03-26 15:38:00 +0100

    moving swagger:blocks into the json_api_resources

--- src/ruby/meetings/app/models/person.rb
+++ src/ruby/meetings/app/models/person.rb
@@ -1,7 +1,6 @@
 ##
 # This class represents a person, who can be a government person or an outside organisation person depending on the meeting/hospitality & etc
 class Person < Entity
-  include Swagger::Blocks
   ##
   # has_many :influence_office_people links to the intermediate table between people and meetings/hospitality/gifts/etc
   has_many :influence_office_people,foreign_key: 'person_id'
@@ -11,50 +10,4 @@ class Person < Entity
   has_many :travels,through: :influence_office_people
 
 
-  swagger_schema :Person do
-      key :type, :object
-      property :attributes do
-        property :name do
-          key :type, :string
-        end
-        property :wikipedia_entry do
-          key :type, :string
-        end
-      end
-      key :required, [:id,:type]
-      property :links do
-        property :self do
-          key :type,:string
-        end
-      end
-      property :relationships do
-        Person.reflections.keys.each do | assoc |
-          property assoc.underscore.downcase.to_sym do
-          property :self do
-            key :type,:string
-          end
-          property :related do
-            key :type,:string
-          end
-        end
-        end
-      end
-  end
-
-
-
-  swagger_schema :PersonInput do
-    allOf do
-      schema do
-        key :'$ref', :Person
-      end
-      schema do
-        key :required, [:name]
-        property :id do
-          key :type, :integer
-          key :format, :int64
-        end
-      end
-    end
-  end
 end
